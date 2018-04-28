8b47df3cd9b33c6ccd959df842a5df5ed747b317
Ranjith-achar <ranjith.chandrachar@gmail.com>
2018-04-28 21:22:13 +0530

    first controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class UserController extends BaseController
+{
+
+    public function index()
+    {
+        return "uywswus";
+    }
+}
---
+++ app/Http/Controllers/newcontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class newcontroller extends Controller
+{
+    public function index(){
+      return 'hello';
+
+    }
+}
